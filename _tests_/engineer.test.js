925dab02a5527cc0798709b15e1e3907627dfce0
Javier Perez <javier21perez98@gmail.com>
2021-04-13 20:12:39 -0700

    added engineer class

--- _tests_/engineer.test.js
+++ _tests_/engineer.test.js
@@ -1,1 +1,21 @@
-test.skip("engineer smoke test", () => {});
+const Engineer = require("../lib/engineer");
+
+describe("Engineer", () => {
+	// add GitHub to engineer
+	describe("getGitHub", () => {
+		it("Engineer should have a Github profile added", () => {
+			const github = "javierperez98";
+			const engineer = new Engineer("Bob", 1, "bob@gamil.com", github);
+			const engineerGitHub = engineer.getGitHub();
+			expect(engineerGitHub).toBe(github);
+		});
+	});
+	//Change Role to Engineer
+	describe("getRole", () => {
+		it("Employee role should be replaced with Engineer role", () => {
+			const engineer = new Engineer("Bob", 1, "bob@gamil.com", "javierperez98");
+			const engineerRole = engineer.getRole();
+			expect(engineerRole).toBe("Engineer");
+		});
+	});
+});
