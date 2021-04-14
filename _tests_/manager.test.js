d3ce864e13c1cb78e79b8ff49cd20b0132a3f725
Javier Perez <javier21perez98@gmail.com>
2021-04-13 19:57:41 -0700

    added manager role and office number

--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -1,1 +1,23 @@
-test.skip("manager smoke test", () => {});
+const Manager = require("../lib/manager");
+
+describe("Manager", () => {
+	// Office Number
+	describe("getOfficeNumber", () => {
+		it("Manager should have an Office number.", () => {
+			const officeNumber = "561";
+			const manager = new Manager("Bob", 1, "bob@gamil.com", officeNumber);
+			const managerOfficeNumber = manager.getOfficeNumber();
+			expect(managerOfficeNumber).toBe(officeNumber);
+		});
+	});
+
+	// getRole overridden to return Manager
+	describe("getRole", () => {
+		it("Employee role should be replaced with Manager role", () => {
+			const officeNumber = "561";
+			const man = new Manager("Bob", 1, "bob@gamil.com", officeNumber);
+			const manRole = man.getRole();
+			expect(manRole).toBe("Manager");
+		});
+	});
+});
